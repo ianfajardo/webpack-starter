d80d6ff6bc11d7f589c68da94d0f07a9992dd4bd
ARCDS\ifajardo <ifajardo@arrccorp.com>
2017-10-03 11:53:22 -0400

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+  entry: {
+		app: './src/index.js'//,
+		//print: './src/print.js'
+	},
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+	devServer:{
+		contentBase: './dist'
+	},
+  module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: extractSass.extract({
+						use:[{
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }],
+						// use style-loader in development
+						fallback: "style-loader"
+				})
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+  },
+	plugins: [
+				extractSass,
+				new CleanWebpackPlugin(['dist']),
+				new HtmlWebpackPlugin({
+					title: 'Output Management'
+				})
+		
+	]
+};	
